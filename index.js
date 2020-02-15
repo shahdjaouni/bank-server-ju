06bc199c293b4265d0141266a203a1a4e94adaea
shahdjaouni <shahd.jaouni@gmail.com>
2020-02-15 22:30:33 +0200

    lock a file

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const fs = require('fs')
+const { promisify } = require('util')
+const lockfile = require('proper-lockfile');
+const Promise = require('bluebird');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
 
 const accounts = [
     {
@@ -27,22 +33,22 @@ const accounts = [
 
 app.get('/', (req, res) => res.send('Welcome to OS assignment ^_^'))
 
-app.get('/details/:uid', (req, res) => {
+app.get('/details/:uid', async (req, res) => {
     const { uid } = req.params;
-    const account = findAccount(uid);
-    if(!account) return res.status(404).send('User not found');
+    const account = await readFile('./db.txt', uid)
+    if (!account) return res.status(404).send('User not found');
     return res.status(200).send(account);
 });
 
-app.post('/deposit/:uid', (req, res) => {
+app.post('/deposit/:uid', async (req, res) => {
     const { uid } = req.params;
     const { amount } = req.body;
-
+    await writeFile("./db.txt", uid, amount);
     const account = findAccount(uid);
-    if(!account) return res.status(404).send('User not found');
+    if (!account) return res.status(404).send('User not found');
     account.balance += amount
 
-    return res.status(200).send(accounts);
+    return res.status(200).send(account);
 });
 
 app.post('/withdrawl/:uid', (req, res) => {
@@ -51,19 +57,83 @@ app.post('/withdrawl/:uid', (req, res) => {
 
     const account = findAccount(uid);
 
-    if(!account) return res.status(404).send('User not found');
+    if (!account) return res.status(404).send('User not found');
     if (!checkBalance(amount, account)) return res.send('Balance is not enough');
-    
+
     account.balance -= amount;
-    return res.status(200).json(accounts);
+    return res.status(200).json(account);
 });
 
 const checkBalance = (amount, account) => account.balance >= amount;
 
 const findAccount = (uid) => {
-    const users = accounts.filter(acc =>  acc.uid === uid);
+    const users = accounts.filter(acc => acc.uid === uid);
     if (users.length) return users[0];
     return null
+}
+
+const writeFile = (path, uid, amount) => {
+    const retryOptions = {
+        retries: {
+            retries: 5,
+            factor: 3,
+            minTimeout: 1 * 1000,
+            maxTimeout: 60 * 1000,
+            randomize: true,
+        }
+    };
+
+    let cleanup;
+    return Promise.try(() => {
+        return lockfile.lock(path, retryOptions);
+    }).then(async release => {
+        cleanup = release;
+
+        let lines = await readFileAsync(path);
+        lines = lines.toString();
+
+        const line = await findLine(lines, uid);
+        var regex = new RegExp(line);
+
+        const lineData = line.split(' ');
+        lineData[2] = +lineData[2] + amount;
+
+        let replacement = lineData.join(" ");
+        var result = lines.replace(regex, replacement);
+
+        await writeFileAsync(path, result, 'utf8')
+    }).then(() => {
+        console.log('Finished!');
+    }).catch((err) => {
+        console.error(err);
+    }).finally(() => {
+        cleanup && cleanup();
+    });
+}
+
+const findLine = (lines, uid) => {
+    return new Promise(resolve => setTimeout(() => {
+        let result = null;
+        lines.split(/\r?\n/).forEach(function (line) {
+            const lineData = line.split(' ');
+            if (lineData[0] === uid)
+                result = line
+        });
+        resolve(result)
+    }, 500));
+};
+
+const readFile = async (path = './db.txt', uid) => {
+    let account = null;
+
+    const lines = await readFileAsync(path);
+    lines.toString().split(/\r?\n/).forEach(function (line) {
+        const lineData = line.split(' ');
+        if (lineData[0] === uid)
+            account = { uid: lineData[0], name: lineData[1], balance: lineData[2] };
+    });
+
+    return account;
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
